bdcd2c2fb1707891da127f38f5e08aa1a122d3dc
CaioVitor1 <caiovitor33@hotmail.com>
2022-05-07 13:37:30 -0300

    finished stories and ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- src/Left.js
+++ src/Left.js
@@ -1,9 +1,12 @@
 import Stories from "./Stories";
+import Posts from "./Posts";
+
 
 export default function Left() {
     return (
     <div className="esquerda">
         <Stories />
+        <Posts />
     </div>
     )
 }
--- src/Stories.js
+++ src/Stories.js
@@ -1,91 +1,32 @@
-/*function RenderStories(props) {
-    const storiesImg = ["imagens/9gag.svg","imagens/meowed.svg", 
-        "imagens/barked.svg", "imagens/nathanwpylestrangeplanet.svg", "imagens/wawawicomics.svg",
-        "imagens/respondeai.svg","imagens/filomoderna.svg", "imagens/memeriagourmet.svg"];
-    const storiestitle = ["9gag", "meowed", "barked", "nathanwpylestrangeplanet", "wawawicomics",
-        "respondeai","filomoderna", "memeriagourmet" ];
-    return (
 
-        <div className="stories">
-            <div className="story">
-              <div className="imagem"></div>
-              {storiesImg.map(img => <img src={img} />)}
-              <div className="usuario"></div>
-              {storiestitle.map(title => {title})}
-              </div>
-            </div>
-    )      
-}
-*/
+const everyStories = [ 
+{imagem:"imagens/9gag.svg", titulo:"9gag"}, {imagem:"imagens/meowed.svg", titulo: "meowed"},
+{imagem:"imagens/barked.svg", titulo: "barked"}, {imagem:"imagens/nathanwpylestrangeplanet.svg", titulo: "nathanwpylestrangeplanet"},
+{imagem:"imagens/wawawicomics.svg", titulo: "wawawicomics"}, {imagem:"imagens/respondeai.svg", titulo: "respondeai"},
+{imagem:"imagens/filomoderna.svg", titulo: "filomoderna"}, {imagem:"imagens/memeriagourmet.svg", titulo: "memeriagourmet"},
+]
+
+function RenderStories(props) {
+        return (
+
+    <div className="story">
+        <div className="imagem">
+        <img src={props.image} />
+        </div>                                
+        <div className="usuario">
+          {props.title}
+        </div>
+    </div>
+           
+  )       
+} 
 
 export default function Stories() {
     return (
     <div className="stories">
-            
-
-            <div className="story">
-              <div className="imagem">
-                <img src="imagens/meowed.svg" />
-              </div>
-              <div className="usuario">
-                meowed
-              </div>
-            </div>
-
-            <div className="story">
-              <div className="imagem">
-                <img src="imagens/barked.svg" />
-              </div>
-              <div className="usuario">
-                barked
-              </div>
-            </div>
-
-            <div className="story">
-              <div className="imagem">
-                <img src="imagens/nathanwpylestrangeplanet.svg" />
-              </div>
-              <div className="usuario">
-                nathanwpylestrangeplanet
-              </div>
-            </div>
-
-            <div className="story">
-              <div className="imagem">
-                <img src="imagens/wawawicomics.svg" />
-              </div>
-              <div className="usuario">
-                wawawicomics
-              </div>
-            </div>
-            
-            <div className="story">
-              <div className="imagem">
-                <img src="imagens/respondeai.svg" />
-              </div>
-              <div className="usuario">
-                respondeai
-              </div>
-            </div>
-
-            <div className="story">
-              <div className="imagem">
-                <img src="imagens/filomoderna.svg" />
-              </div>
-              <div className="usuario">
-                filomoderna
-              </div>
-            </div>
-
-            <div className="story">
-              <div className="imagem">
-                <img src="imagens/memeriagourmet.svg" />
-              </div>
-              <div className="usuario">
-                memeriagourmet
-              </div>
-            </div>
-
+    
+    {everyStories.map((url) => (<RenderStories image={url.imagem} title={url.titulo}/>))}
+    
             <div className="setinha">
               <ion-icon name="chevron-forward-circle"></ion-icon>
             </div>
